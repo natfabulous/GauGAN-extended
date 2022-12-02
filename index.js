83dc0c5ed23d78af2ac8cf6d30b3b0570e13adb8
Natalie Keenan <natalie@natfabulous.com>
2022-12-02 02:53:24 -0800

    the cli is a mess, but it can take apart a video for you now

--- index.js
+++ index.js
@@ -3,42 +3,51 @@ const fs = require('fs')
 const { program } = require('commander');
 const log = require('./logger')
 const InferenceSession = require('./fetch')
+const {mkdir} = require('./utils/filenameplus')
+const videoToFrames = require('./utils/ffmpegUtils')
+const progressBar = require("progress-bar-cli");
 
 // Command line args
 program
-  .option('-d, --debug', 'output extra debugging')
-  .option('-i, --input', 'input directory for the shit you want to send to the gaugan')
+  .option('-v, --debug', 'output extra debugging')
+  .option('-i, --video [string]', 'input file for use with video')
+  .option('-w, --working [string]', 'working directory where intermediary info is saved')
+  .option('-d, --directory [string]', 'input directory for image sequences')
+  .option('-o, --output [string]', 'output directory')
 program.parse(process.argv);
 
+// indir may not be set yet
 const options = program.opts();
 if (options.debug) log.level = 'debug';
 
-
 // Actual program
-
-// get Files 
-let indir = options.input
-let outdir = indir + "\\out"
-const dir = fs.opendirSync(dirname)
-let files = [], e
-while ((e = dir.readSync()) !== null) { files.push(dirname+'\\'+e.name)}
-dir.closeSync()
-files.sort((a, b) => a.localeCompare(b, undefined, { numeric: true }));
-log.log(files)
-/////////
 ;(async () => {
+  if (options.working) mkdir(options.working)
+  if (options.video){
+    await videoToFrames(options.video,options.working)
+  }
+  // wrangle input images
+  let imagedir = options.working ? options.working : options.directory
+  let outdir = options.output
+  const dir = fs.opendirSync(imagedir)
+  let files = [], e
+  while ((e = dir.readSync()) !== null) { files.push(imagedir+'\\'+e.name)}
+  dir.closeSync()
+  files.sort((a, b) => a.localeCompare(b, undefined, { numeric: true }));
+  log.log(files)
+  log.log('Creating InferenceSession')
   let is = new InferenceSession(new Date().toISOString().slice(0, -5).replaceAll(':', '_'))
 
+  let t0 = new Date()
   for (const [i,e] of files.entries()) {
+    progressBar.progressBar(i, files.length, t0, {style:4});
     is.segmap=fs.readFileSync(e)
     let res = await is.infer()
     let blob = await res.blob()
     let abuff = await blob.arrayBuffer();
     let uarr = new Uint8Array(abuff)
     let buff = Buffer.from(uarr)
-    if (!fs.existsSync(`${outdir}`)){
-      fs.mkdirSync(`${outdir}`);
-    }
+    mkdir(outdir)
     fs.writeFileSync(`${outdir}/${i+1}.jpg`, buff)
   }
 })()
--- utils/ffmpegUtils.js
+++ utils/ffmpegUtils.js
@@ -1,22 +1,58 @@
 //https://www.gyan.dev/ffmpeg/builds/
 const execa = require("execa")
 const log = require('../logger')
+const path = require('path');
+const {mkdir, extendPath, extendName} = require('./filenameplus')
+const ffmpeg = require('ffmpeg-static');
+const ffprobe = require('ffprobe-static').path;
 
-const BIN = require('ffmpeg-static');
-const tryExeca = async (cmd) => {
+const tryExeca = async (BIN, cmd) => {
   try {
-    log.debug(`ffmpegUtils.exe tryExeca cmd: ${cmd}`)
-    await execa(BIN,cmd)
+    cmd = ['-v','quiet'].concat(cmd) //no stderr
+    if(BIN==ffmpeg) cmd=cmd.concat(['-y']) 
+    log.log(`ffmpegUtils.exe tryExeca cmd: ${cmd}`)
+    return await execa(BIN,cmd)
   } catch (err) {
     log.error(err)
   }
 }
 
+const getVideoInfo = async(infile)=>{
+  let x = await tryExeca (ffprobe,
+  [
+    '-print_format',
+    'json',
+    '-show_format',
+    '-show_streams',
+    infile
+  ])
+  log.debug(x.stdout)
+  return JSON.parse(x.stdout)
+}
 
-(async () => {
-  await tryExeca([
-        '-i',
-        '../src/test.jpg',
-        '../out/test.png',
-      ])
-})()
+// TODO crop and turn to images in same step
+const cropVideoSquare = async (infile,ofile,vinfo)=>{
+  let width = vinfo.width
+  let height = vinfo.height
+  let cmd = [
+    '-i',
+    infile,
+    '-vf',
+    width<height ? 'crop=iw:iw' : 'crop=ih:ih',
+    ofile
+  ]
+  await tryExeca(ffmpeg,cmd)
+}
+const videoToFrames = async (infile,outdir)=>{
+  let vinfo = await getVideoInfo(infile)
+  let croppedPath = extendPath(extendName(infile,'-cropped'),'intermediary')
+  await cropVideoSquare(infile,croppedPath,vinfo)
+  cmd = [
+    '-i',
+    croppedPath,
+    outdir+'/%d.png'
+  ]
+  await tryExeca(ffmpeg,cmd)
+}
+
+module.exports = videoToFrames
